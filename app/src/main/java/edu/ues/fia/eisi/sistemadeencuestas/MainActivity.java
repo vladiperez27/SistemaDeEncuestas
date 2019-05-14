fcfdabaaf945ff2cfe1f14589ba237b7d3f9ff2f
LLuucchhoo <lcortezdominguez@hotmail.com>
2019-05-14 10:32:01 -0600

    Update MainActivity.java

--- app/src/main/java/edu/ues/fia/eisi/sistemadeencuestas/MainActivity.java
+++ app/src/main/java/edu/ues/fia/eisi/sistemadeencuestas/MainActivity.java
@@ -1,5 +1,9 @@
 package edu.ues.fia.eisi.sistemadeencuestas;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +12,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et_nombre,et_apellido;
+    Button bt_guardar,bt_mostrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,37 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        et_nombre= (EditText) findViewById(R.id.et_nombre);
+        et_apellido= (EditText) findViewById(R.id.et_apellido);
+        bt_guardar= (Button) findViewById(R.id.bt_guardar);
+        bt_mostrar= (Button) findViewById(R.id.bt_mostrar);
+        bt_guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guardar(et_nombre.getText().toString(),et_apellido.getText().toString());
+            }
+        });
+        bt_mostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Listado.class));
+            }
+        });
+    }
+    private void guardar(String nombre, String apellido){
+        BaseHelper helper =new BaseHelper(this,"sistemadeencuesta",null,1);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        try{
+            ContentValues c= new ContentValues();
+            c.put("nomalumno",nombre);
+            c.put("apelalumno",apellido);
+            db.insert("ALUMNO",null, c);
+            db.close();
+            Toast.makeText(this,"Registro insertado",Toast.LENGTH_SHORT).show();
+
+        }catch(Exception e){
+            Toast.makeText(this,"Error: "+e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
